d4b0a52275ba304735de974a1e95c243618c14c1
TthanhGithub <kaiapussale@gmail.com>
2025-10-17 18:27:13 +0700

    Create DaHood.lua

---
+++ DaHood.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/Rm8X3Btf", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+loadstring(game:HttpGet("https://pastebin.com/raw/n4E57yge"))()
