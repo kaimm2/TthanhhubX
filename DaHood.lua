50993c125ef176f3c5a24bdc1d1fa10a7ca7d31d
TthanhGithub <kaiapussale@gmail.com>
2025-10-17 18:32:15 +0700

    Update DaHood.lua

--- DaHood.lua
+++ DaHood.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://pastebin.com/raw/Rm8X3Btf", true))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-loadstring(game:HttpGet("https://pastebin.com/raw/n4E57yge"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kaimm2/TthanhhubX/refs/heads/main/Camlock"))()
