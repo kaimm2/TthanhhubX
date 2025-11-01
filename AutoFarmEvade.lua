80201211c14a86bcaefd81436b4731f96a158c7c
TthanhGithub <kaiapussale@gmail.com>
2025-11-02 06:30:17 +0700

    Create AutoFarmEvade.lua

---
+++ AutoFarmEvade.lua
@@ -0,0 +1,6 @@
+getgenv().farmTickets = true
+getgenv().farmRevives = false
+getgenv().noRender = false
+getgenv().TrackMePlease = true
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kaimm2/TthanhhubX/refs/heads/main/AutoFarmSource'))()
